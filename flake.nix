6d31ffbe2f7ef5eff0b6191f3a7a93816babf2ba
Victor Payno <vpayno@users.noreply.github.com>
2025-04-28 23:46:40 -0700

    nix: add default develop shell

--- flake.nix
+++ flake.nix
@@ -28,9 +28,48 @@
       system:
       let
         pkgs-unstable = nixpkgs-unstable.legacyPackages.${system};
+
+        kernelDevTools = (
+          with pkgs-unstable;
+          [
+            bc
+            bintools
+            bison
+            cargo
+            clang
+            cpio
+            elfutils
+            flex
+            gitFull
+            global
+            gmp
+            gnumake
+            kmod
+            libmpc
+            llvm
+            mpfr
+            ncurses
+            nettools
+            # openssl
+            pahole
+            perl
+            python3Minimal
+            rsync
+            rustc
+            ubootTools
+            zlib
+            zstd
+          ]
+        );
       in
       {
         formatter = treefmt-conf.formatter.${system};
+
+        devShells = {
+          default = pkgs-unstable.mkShell {
+            packages = kernelDevTools;
+          };
+        };
       }
     );
 }
